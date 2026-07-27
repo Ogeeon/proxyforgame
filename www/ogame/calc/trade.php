6301081e85884bdd88a6237395e469683de0002e
Alexey Bogatyrev <avbogatyrev@gmail.com>
2026-07-27 17:15:07 +1000

    style(trade): use spaces for indentation and drop closing PHP tag

--- www/ogame/calc/trade.php
+++ www/ogame/calc/trade.php
@@ -11,14 +11,12 @@ $l = Intl::getTranslations($lang, 'trade');
 $countries = SqlQuery("SELECT c.lang2 as lang, c.name as name, s.server as server FROM countries AS c INNER JOIN servers as s ON c.lang2 = s.lang WHERE c.lang=?", array($lang));
 $universes = array();
 if ($countries) {
-	foreach ($countries as $row) {
-		$r = SqlQuery("SELECT server, name FROM universes WHERE lang = ? ORDER BY name", array($row['lang']));
-		if ($r === false) {
-			continue;
-		}
-		$universes[$row['lang']] = $r;
-	}
+    foreach ($countries as $row) {
+        $r = SqlQuery("SELECT server, name FROM universes WHERE lang = ? ORDER BY name", array($row['lang']));
+        if ($r === false) {
+            continue;
+        }
+        $universes[$row['lang']] = $r;
+    }
 }
 require_once 'trade.tpl';
-
-?>
