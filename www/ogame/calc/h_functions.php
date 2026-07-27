4521fac328aca951ca25abc4f0bcfce5e0f4a764
Alexey Bogatyrev <avbogatyrev@gmail.com>
2026-07-27 19:32:11 +1000

    fix(expeditions): add braces around nested statements in h_functions.php

--- www/ogame/calc/h_functions.php
+++ www/ogame/calc/h_functions.php
@@ -1,6 +1,6 @@
 <?php
 function GetServerData($strUni, $strDomain, $strTime) {
-    if (!isset($strTime)) $strTime = 1;
+    if (!isset($strTime)) { $strTime = 1; }
     $varReturn = false;
     $upDBUni = false;
     $xmlFile = 'https://logserver.net/xml/' . $strDomain . '/serverData/' . $strUni . '.xml';
@@ -26,45 +26,46 @@ function GetServerData($strUni, $strDomain, $strTime) {
 
     if ($xml != false) {
         foreach ($xml->children() as $key => $serverData) {
-            if ($key == "name") 									$varReturn["name"] = (string) $serverData; 
-            if ($key == "speed") 									$varReturn["speed"] = (string) $serverData; 
-            if ($key == "speedFleet") 								$varReturn["speedFleet"] = (string) $serverData; 
-            if ($key == "galaxies") 								$varReturn["galaxies"] = (string) $serverData; 
-            if ($key == "systems") 									$varReturn["systems"] = (string) $serverData; 
-            if ($key == "acs") 										$varReturn["acs"] = (string) $serverData; 
-            if ($key == "rapidFire") 								$varReturn["rapidFire"] = (string) $serverData; 
-            if ($key == "defToTF") 									$varReturn["defToTF"] = (string) $serverData; 
-            if ($key == "debrisFactor") 							$varReturn["debrisFactor"] = (string) $serverData; 
-            if ($key == "debrisFactorDef") 							$varReturn["debrisFactorDef"] = (string) $serverData; 
-            if ($key == "repairFactor") 							$varReturn["repairFactor"] = (string) $serverData; 
-            if ($key == "newbieProtectionLimit") 					$varReturn["newbieProtectionLimit"] = (string) $serverData; 
-            if ($key == "newbieProtectionHigh") 					$varReturn["newbieProtectionHigh"] = (string) $serverData; 
-            if ($key == "topScore") 								$varReturn["topScore"] = (string) $serverData; 
-            if ($key == "bonusFields") 								$varReturn["bonusFields"] = (string) $serverData; 
-            if ($key == "donutGalaxy") 								$varReturn["donutGalaxy"] = (string) $serverData; 
-            if ($key == "donutSystem") 								$varReturn["donutSystem"] = (string) $serverData; 
-            if ($key == "wfEnabled") 								$varReturn["wfEnabled"] = (string) $serverData; 
-            if ($key == "wfMinimumRessLost") 						$varReturn["wfMinimumRessLost"] = (string) $serverData; 
-            if ($key == "wfMinimumLossPercentage") 					$varReturn["wfMinimumLossPercentage"] = (string) $serverData; 
-            if ($key == "wfBasicPercentageRepairable") 				$varReturn["wfBasicPercentageRepairable"] = (string) $serverData; 
-            if ($key == "globalDeuteriumSaveFactor") 				$varReturn["globalDeuteriumSaveFactor"] = (string) $serverData; 
-            if ($key == "bashlimit") 								$varReturn["bashlimit"] = (string) $serverData; 
-            if ($key == "probeCargo") 								$varReturn["probeCargo"] = (string) $serverData; 
-            if ($key == "researchDurationDivisor") 					$varReturn["researchDurationDivisor"] = (int) $serverData; 
-            if ($key == "marketplaceBasicTradeRatioMetal") 			$varReturn["marketplaceBasicTradeRatioMetal"] = (float) $serverData; 
-            if ($key == "marketplaceBasicTradeRatioCrystal") 		$varReturn["marketplaceBasicTradeRatioCrystal"] = (float) $serverData; 
-            if ($key == "marketplaceBasicTradeRatioDeuterium") 		$varReturn["marketplaceBasicTradeRatioDeuterium"] = (float) $serverData; 
-            if ($key == "marketplaceTaxNotSold") 					$varReturn["marketplaceTaxNotSold"] = (float) $serverData; 
+            if ($key == "name") { $varReturn["name"] = (string) $serverData; }
+            if ($key == "speed") { $varReturn["speed"] = (string) $serverData; }
+            if ($key == "speedFleet") { $varReturn["speedFleet"] = (string) $serverData; }
+            if ($key == "galaxies") { $varReturn["galaxies"] = (string) $serverData; }
+            if ($key == "systems") { $varReturn["systems"] = (string) $serverData; }
+            if ($key == "acs") { $varReturn["acs"] = (string) $serverData; }
+            if ($key == "rapidFire") { $varReturn["rapidFire"] = (string) $serverData; }
+            if ($key == "defToTF") { $varReturn["defToTF"] = (string) $serverData; }
+            if ($key == "debrisFactor") { $varReturn["debrisFactor"] = (string) $serverData; }
+            if ($key == "debrisFactorDef") { $varReturn["debrisFactorDef"] = (string) $serverData; }
+            if ($key == "repairFactor") { $varReturn["repairFactor"] = (string) $serverData; }
+            if ($key == "newbieProtectionLimit") { $varReturn["newbieProtectionLimit"] = (string) $serverData; }
+            if ($key == "newbieProtectionHigh") { $varReturn["newbieProtectionHigh"] = (string) $serverData; }
+            if ($key == "topScore") { $varReturn["topScore"] = (string) $serverData; }
+            if ($key == "bonusFields") { $varReturn["bonusFields"] = (string) $serverData; }
+            if ($key == "donutGalaxy") { $varReturn["donutGalaxy"] = (string) $serverData; }
+            if ($key == "donutSystem") { $varReturn["donutSystem"] = (string) $serverData; }
+            if ($key == "wfEnabled") { $varReturn["wfEnabled"] = (string) $serverData; }
+            if ($key == "wfMinimumRessLost") { $varReturn["wfMinimumRessLost"] = (string) $serverData; }
+            if ($key == "wfMinimumLossPercentage") { $varReturn["wfMinimumLossPercentage"] = (string) $serverData; }
+            if ($key == "wfBasicPercentageRepairable") { $varReturn["wfBasicPercentageRepairable"] = (string) $serverData; }
+            if ($key == "globalDeuteriumSaveFactor") { $varReturn["globalDeuteriumSaveFactor"] = (string) $serverData; }
+            if ($key == "bashlimit") { $varReturn["bashlimit"] = (string) $serverData; }
+            if ($key == "probeCargo") { $varReturn["probeCargo"] = (string) $serverData; }
+            if ($key == "researchDurationDivisor") { $varReturn["researchDurationDivisor"] = (int) $serverData; }
+            if ($key == "marketplaceBasicTradeRatioMetal") { $varReturn["marketplaceBasicTradeRatioMetal"] = (float) $serverData; }
+            if ($key == "marketplaceBasicTradeRatioCrystal") { $varReturn["marketplaceBasicTradeRatioCrystal"] = (float) $serverData; }
+            if ($key == "marketplaceBasicTradeRatioDeuterium") { $varReturn["marketplaceBasicTradeRatioDeuterium"] = (float) $serverData; }
+            if ($key == "marketplaceTaxNotSold") { $varReturn["marketplaceTaxNotSold"] = (float) $serverData; }
 
-            if ($key == "speedFleetPeaceful") 						$varReturn["speedFleetPeaceful"] = (int) $serverData; 
-            if ($key == "speedFleetWar") 							$varReturn["speedFleetWar"] = (int) $serverData; 
-            if ($key == "speedFleetHolding") 						$varReturn["speedFleetHolding"] = (int) $serverData;
-            
-            if ($key == "deuteriumInDebris") 						$varReturn["deuteriumInDebris"] = (int) $serverData; 	        	
-            if ($key == "fleetIgnoreEmptySystems") 					$varReturn["fleetIgnoreEmptySystems"] = (int) $serverData; 	        	
+            if ($key == "speedFleetPeaceful") { $varReturn["speedFleetPeaceful"] = (int) $serverData; }
+            if ($key == "speedFleetWar") { $varReturn["speedFleetWar"] = (int) $serverData; }
+            if ($key == "speedFleetHolding") { $varReturn["speedFleetHolding"] = (int) $serverData; }
+
+            if ($key == "deuteriumInDebris") { $varReturn["deuteriumInDebris"] = (int) $serverData; }
+            if ($key == "fleetIgnoreEmptySystems") { $varReturn["fleetIgnoreEmptySystems"] = (int) $serverData; }
         }
+    } else {
+        return false;
     }
-    else return false;
 
     //if ($upDBUni) cDB::saveUniverses($strUni, $strDomain, $varReturn);
     return $varReturn;
